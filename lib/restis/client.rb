7ac64f1c50f7bdc5fb808a808ed0b0f2059b451e
Douglas Campos <douglas@theros.info>
2010-08-31 18:25:26 -0300

    redis should use same args on internal reconnections

--- lib/restis/client.rb
+++ lib/restis/client.rb
@@ -24,11 +24,11 @@ module Restis
 				end
 				@redis.set(key, queue_size)
 			end
-			redis = Redis.new(@args.merge(:timeout => 0))
+			redis = Redis.new(:timeout => 0)
 			redis.subscribe(channel) do |on|
 				on.message do |channel, msg|
 					block.call(redis, channel, msg)
-					@redis.incr(key)
+					Redis.new(@args).incr(key)
 				end
 			end
 		end
